90a0467394a24b3f6e6cc0f7404f2d13e817bbb3
Kaplaugher <kaplaugher94@gmail.com>
2017-01-13 15:50:33 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import SingleMovie from './SingleMovie'
+
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+ReactDOM.render(
+
+	<Router history={hashHistory}>
+		<Route path="/" component={App} />
+		<Route path="/:movie" component={SingleMovie} />
+		<Route path="/:movie/:cast" component={SingleMovie} />
+
+	</Router>,
+  document.getElementById('root')
+);
